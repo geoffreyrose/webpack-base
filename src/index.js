10e42031679bb47df2d354938c787ab673c1a316
Geoffrey Rose <geoffrey@geoffreyrosedesign.com>
2018-01-21 18:07:40 -0800

    compiles scss/css and js

---
+++ src/index.js
@@ -0,0 +1,4 @@
+//import _ from 'lodash';
+var $ = require("jquery");
+require( './css/scss/style-test.css');
+require( './css/scss/styles.scss');
---
+++ webpack.config.js
@@ -0,0 +1,140 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractCSS = new ExtractTextPlugin("css-style.css");
+const ExtractSCSS = new ExtractTextPlugin("scss-style.css");
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = (env = {}) => {
+    const isProduction = env.production === true;
+    // the clean options to use
+    let pathsToClean = [
+        'dist',
+        'build'
+    ]
+
+    let cleanOptions = {
+        root: '',
+        exclude: [],
+        verbose: true,
+        dry: false
+    }
+
+
+    return {
+        devtool: (() => {
+            if (isProduction) return 'hidden-source-map'
+            else return 'source-map'
+        })(),
+        entry: {
+            'app.js': './src/index.js'
+        },
+        output: {
+            filename: '[name]',
+            path: path.resolve(__dirname, 'dist')
+        },
+        module: {
+            rules: [{
+                    test: /\.scss$/,
+                    use: ExtractSCSS.extract({
+                        fallback: 'style-loader',
+                        use: [{
+                                loader: 'css-loader',
+
+                                options: (() => {
+                                    if (isProduction) {
+                                        return {
+                                            minimize: true || { /* CSSNano Options */ },
+                                            includePaths: [
+                                                path.resolve("node_modules/foundation-sites/scss")
+                                            ]
+                                        }
+                                    } else {
+                                        return {
+                                            sourceMap: true,
+                                            includePaths: [
+                                                path.resolve("node_modules/foundation-sites/scss")
+                                            ]
+                                        }
+                                    }
+                                })()
+                            },
+                            {
+                                loader: 'sass-loader',
+                                options: (() => {
+                                    if (isProduction) {
+                                        return {
+                                            minimize: true || { /* CSSNano Options */ },
+                                            includePaths: [
+                                                path.resolve("node_modules/foundation-sites/scss")
+                                            ]
+                                        }
+                                    } else {
+                                        return {
+                                            sourceMap: true,
+                                            includePaths: [
+                                                path.resolve("node_modules/foundation-sites/scss")
+                                            ]
+                                        }
+                                    }
+                                })()
+                            }
+                        ],
+                    })
+                },
+                {
+                    test: /\.css$/,
+                    use: ExtractCSS.extract({
+                        fallback: "style-loader",
+                        use: [{
+                            loader: 'css-loader',
+                            options: (() => {
+                                if (isProduction) {
+                                    return {
+                                        minimize: true || { /* CSSNano Options */ }
+                                    }
+                                }
+                            })()
+                        }, ]
+                    })
+                },
+                {
+                    test: /\.(png|svg|jpg|gif)$/,
+                    use: [
+                        'file-loader'
+                    ]
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['env']
+                        }
+                    }
+                }
+            ]
+        },
+
+        plugins: (() => {
+            if (isProduction) {
+                return [
+                    ExtractCSS,
+                    ExtractSCSS,
+                    new UglifyJSPlugin({
+                        sourceMap: true
+                    }),
+                    new CleanWebpackPlugin(pathsToClean, cleanOptions),
+                ]
+            } else {
+                return [
+                    ExtractCSS,
+                    ExtractSCSS,
+                    new CleanWebpackPlugin(pathsToClean, cleanOptions),
+                ]
+            }
+        })(),
+
+    }
+}
